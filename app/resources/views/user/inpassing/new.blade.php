c2016afe5e4c889e74ac0f4235154c99d056a7e5
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-02-07 15:29:28 +0800

    fix folder structure for inpassing

--- app/resources/views/user/inpassing/new.blade.php
+++ app/resources/views/user/inpassing/new.blade.php
@@ -1,31 +1,16 @@
 @extends('adminlte::page')
 
-@section('title', 'Profile | Inpassing')
+@section('title_prefix', 'Inpassing')
 
 @section('content_header')
-    <div class="d-flex">
-        <a href="{{ route('user.home') }}" class="btn p-0">
-            <h6>Beranda</h6>
-        </a>
-
-        <h6 class="pl-2 pr-2">/</h6>
-
-        <a href="{{ route('user.inpassing') }}" class="btn p-0">
-            <h6>Profil</h6>
-        </a>
-
-        <h6 class="pl-2 pr-2">/</h6>
-
-        <a href="{{ route('user.inpassing') }}" class="btn p-0">
-            <h6>Inpassing</h6>
-        </a>
-
-        <h6 class="pl-2 pr-2">/</h6>
-
-        <a href="" class="btn p-0">
-            <h6>Tambah</h6>
-        </a>
-    </div>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('user.home') }}">Beranda</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('user.inpassing') }}">Profile</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('user.inpassing') }}">Inpassing</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Tambah</li>
+    </ol>
+</nav>
 @stop
 
 @section('content')
